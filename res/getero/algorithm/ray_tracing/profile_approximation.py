Fix is_half counting of average normal angle

res/getero/algorithm/ray_tracing/profile_approximation.py
@@ -49,21 +49,29 @@ def give_part_of_border(border_arr, curr_segment, is_half, num_one_side_points):
                 end_x, end_y = new_x, new_y
                 X[num_one_side_points + 1 + (i + 1)] = end_x + 0.5
                 Y[num_one_side_points + 1 + (i + 1)] = end_y + 0.5
-            else:
+            elif not reach_left_side:
                 # print("---")
                 print("ГОООЛ!")
                 reach_left_side = True
-                ind_ls = i - 1
-                mirror_point_x = end_x
-                end_x, end_y = border_arr[end_x, end_y, 1], border_arr[end_x, end_y, 2]
+                ind_ls = i - 2
+
+                end_x1, end_y1 = border_arr[end_x, end_y, 1], border_arr[end_x, end_y, 2]
+                end_x, end_y = end_x1, end_y1
+
+                end_x1, end_y1 = border_arr[end_x, end_y, 1], border_arr[end_x, end_y, 2]
+                end_x, end_y = end_x1, end_y1
+
+                mirror_point_x = end_x + 0.5
+
         if reach_left_side:
             while ind_ls < num_one_side_points:
                 new_x, new_y = border_arr[end_x, end_y, 1], border_arr[end_x, end_y, 2]
-                # print(border_arr[end_x, end_y], end_x, end_y)
+                print(border_arr[end_x, end_y], mirror_point_x, end_y + 0.5, mirror_point_x * 2 - end_x + 0.5)
                 X[num_one_side_points + 1 + (ind_ls + 1)] = mirror_point_x * 2 - end_x + 0.5
                 Y[num_one_side_points + 1 + (ind_ls + 1)] = end_y + 0.5
                 end_x, end_y = new_x, new_y
                 ind_ls += 1
+
     else:
         for i in range(num_one_side_points):
             new_x, new_y = border_arr[end_x, end_y, 3], border_arr[end_x, end_y, 4]
@@ -138,7 +146,7 @@ def count_norm_angle(border_arr, cross_vec, curr_segment, is_half, num_one_side_
         norm_angle = start_norm_angle
     else:
         norm_angle = (start_norm_angle+np.pi)%(2*np.pi)
-    return norm_angle, deb, bX, bY, w[0], w[1]#, reach_left_side
+    return norm_angle, deb, bX, bY, w[0], w[1], reach_left_side
 
 
 @clever_njit(do_njit=do_njit, cache=cache, parallel=parallel)

res/getero/tests/tests_internal_funcks/test_profile_approximation.py
@@ -82,10 +82,10 @@ def test_prof_approx(curr_wafer, num_particles, num_one_side_points):
             err_x.append(coll_vec[0])
             err_y.append(coll_vec[1])
             #print("--- ", coll_vec)
-            n_angle, deb, bX, bY, A, B = count_norm_angle(curr_wafer.border_arr, coll_vec, start_segment,
+            n_angle, deb, bX, bY, A, B, reach_left_side = count_norm_angle(curr_wafer.border_arr, coll_vec, start_segment,
                                                           curr_wafer.is_half, num_one_side_points=num_one_side_points)
 
-            #ax.plot(bX, bY, ".")
+
 
             #alpha = 2.0 / (max(np.abs(A), np.abs(B)))
             #x = alpha * np.arange(-1, 1, 0.1) * B + coll_vec[0]
@@ -126,19 +126,33 @@ def test_prof_approx(curr_wafer, num_particles, num_one_side_points):
                 plt.show()
             if len(err_x)%5==0 and False:
                 ax = None
+                ax.plot(bX, bY, ".")
                 ax = plot_wafer(curr_wafer, ax)
                 for i in range(len(err_x)-1):
                     ax.plot([err_x[i], err_x[i+1]], [err_y[i], err_y[i+1]], color=(0, 0, 1, 0.5))
                 ax.set_xlim([min(err_x) - 5, max(err_x) + 5])
                 ax.set_ylim([max(err_y) + 5, min(err_y) - 5])
                 plt.show()
+
             color = "g"
+            #ax.plot(bX, bY, ".")
             ax.plot([curr_vec[0], coll_vec[0]], [curr_vec[1], coll_vec[1]], color=(0, 0, 1, 0.5))
 
             color = "k"
             ax.plot([coll_vec[0], coll_vec[0] + 5 * np.sin(n_angle)],
                     [coll_vec[1], coll_vec[1] + 5 * np.cos(n_angle)], color=color)
 
+            if reach_left_side and False:
+                color = "g"
+                ax.plot(bX, bY, ".")
+                ax.plot([curr_vec[0], coll_vec[0]], [curr_vec[1], coll_vec[1]], color=(0, 0, 1, 0.5))
+
+                color = "k"
+                ax.plot([coll_vec[0], coll_vec[0] + 5 * np.sin(n_angle)],
+                        [coll_vec[1], coll_vec[1] + 5 * np.cos(n_angle)], color=color)
+                plt.show()
+                ax = None
+                ax = plot_wafer(curr_wafer, ax)
 
             if is_oob and False:
 
@@ -169,11 +183,12 @@ def test_prof_approx(curr_wafer, num_particles, num_one_side_points):
 
 end_wafer = Wafer()
 end_wafer.load("../files/wafer_2000.zip")
-#end_wafer.make_half()
+
 #end_wafer = create_test_wafer(num_del=500)
-#end_wafer.save("../files/test_wafer_500del.zip")
-end_wafer.load("../files/test_wafer_5000del.zip")
+end_wafer.save("../files/test_wafer_500del.zip")
+#end_wafer.load("../files/test_wafer_15000del.zip")
 end_wafer.check_self_intersection()
+end_wafer.make_half()
 f = generate_figure(end_wafer, wafer_curr_type="is_cell", do_plot_line=True)
 plt.show()
-test_prof_approx(end_wafer, 2000, 7)
+test_prof_approx(end_wafer, 50, 10)